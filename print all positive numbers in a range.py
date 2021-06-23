ceae18a5dec5cde16c30fdda4155522681c38766
VargheseYagapparaj <76810977+VargheseYagapparaj@users.noreply.github.com>
2021-06-23 15:30:47 +0530

    Add files via upload

---
+++ print all positive numbers in a range.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1=[12,-7,5,64,-14] #giving input 
+
+
+# In[2]:
+
+
+# iterating each number in list
+for num in list1:  #looping statement for checking all numbers
+      
+    if num >= 0:  # checking condition
+       print(num, end = " ")  #printing output
+
+
+# In[5]:
+
+
+list2=[12,14,-95,3]   #giving input
+
+
+# In[6]:
+
+
+# iterating each number in list
+for num in list2:     #looping statement for checking all numbers
+      
+    if num >= 0:       # checking condition
+       print(num, end = " ")    #printing output
+
